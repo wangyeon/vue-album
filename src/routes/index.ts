647fbeedf689760cc3435664adfc4647d2b35b59
wangyeon <mirr2336@gmail.com>
2026-05-04 17:32:49 +0900

    workflows

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import { createWebHistory, createRouter, type RouteRecordRaw } from "vue-router";
 
-import HomeView from "/vue-album/src/views/Home.vue";
+import HomeView from "../views/Home.vue";
 
 const routes: Array<RouteRecordRaw> = [{ path: "/", component: HomeView}];
 
